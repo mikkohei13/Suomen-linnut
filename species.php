a95b622c554289c88ba0842ab8d02ece3ab19154
Mikko Heikkinen <hawk@biomi.org>
2016-01-26 14:01:22 +0200

    Init class

---
+++ species.php
@@ -0,0 +1,15 @@
+<?php
+
+// base class with member properties and methods
+class Species {
+
+   var $something;
+
+   function __construct()
+   {
+       $this->something = "FOO";
+   }
+
+
+
+} // end of class Vegetable
