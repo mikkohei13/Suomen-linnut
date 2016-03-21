1a8dfabc4c8e8fc835ddb54428d5f02fae59d118
Mikko Heikkinen <hawk@biomi.org>
2016-03-21 20:26:11 +0200

    htmlspecialchars for logged data

--- logger.php
+++ logger.php
@@ -1,20 +1,26 @@
 <?php
 
+$logArray = Array();
+foreach ($_POST as $key => $value)
+{
+	$logArray[$key] = htmlspecialchars($value);
+}
+
 // TODO: security
 $logString =  
-	$_POST['ip'] . "\t" . 
-	$_POST['datetime'] . "\t" . 
-	$_POST['error'] . "\t" . 
-	$_POST['latitude'] . "\t" . 
-	$_POST['longitude'] . "\t" . 
-	$_POST['N'] . "\t" . 
-	$_POST['E'] . "\t" . 
-	$_POST['accuracy'] . "\t" . 
-	$_POST['altitude'] . "\t" . 
-	$_POST['altitudeAccuracy'] . "\t" .
-	$_POST['userAgent'] . "\t" .
-	$_POST['innerWidth'] . "\t" .
-	$_POST['innerHeight'] . "\t" .
+	$logArray['ip'] . "\t" . 
+	$logArray['datetime'] . "\t" . 
+	$logArray['error'] . "\t" . 
+	$logArray['latitude'] . "\t" . 
+	$logArray['longitude'] . "\t" . 
+	$logArray['N'] . "\t" . 
+	$logArray['E'] . "\t" . 
+	$logArray['accuracy'] . "\t" . 
+	$logArray['altitude'] . "\t" . 
+	$logArray['altitudeAccuracy'] . "\t" .
+	$logArray['userAgent'] . "\t" .
+	$logArray['innerWidth'] . "\t" .
+	$logArray['innerHeight'] . "\t" .
 	" ";
 
 file_put_contents("logs/js-log.txt", ($logString . "\n"), FILE_APPEND);
