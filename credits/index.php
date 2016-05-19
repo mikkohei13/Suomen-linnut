461e65ac3f731fb75ed7de910d597ae49e35a962
Mikko Heikkinen <hawk@biomi.org>
2016-05-19 22:26:45 +0300

    Remove scripts from credit page

--- credits/index.php
+++ credits/index.php
@@ -47,11 +47,6 @@
             </footer>
         </div>
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="../js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
-
-        <script src="../js/main.js"></script>
-
         <?php include_once "../../../googleanalytics.html"; ?>
 
     </body>
